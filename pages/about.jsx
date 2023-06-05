d7f9cc575f71c93ab6e7540b28850ea44af97704
Jean <jirarrazabal@falabella.cl>
2023-06-05 16:22:31 -0400

    vercel

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,6 +4,7 @@ import { DarkLayout } from '../components/layouts/DarkLayout'
 import { MainLayout } from '../components/layouts/MainLayout'
 
 
+
 export default function AboutPage() {
   return (
     <>
